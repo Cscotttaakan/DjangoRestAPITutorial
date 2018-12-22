9fe67d315b257a8d6584c5cfc18ff8339ac46e5f
Craig Scott <cscott_taakan@yahoo.com>
2018-12-22 15:40:25 -0600

    Finished tutorial

--- api/tests.py
+++ api/tests.py
@@ -40,15 +40,6 @@ class ViewTestCase(TestCase):
         res = new_client.get('/bucketlists/', kwargs={'pk' : 3}, format="json")
         self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_api_can_get_a_bucketlist(self):
-        bucketlist = get_object_or_404(Bucketlist, id=1) 
-        response = self.client.get(
-            '/bucketlists/',
-            kwargs={'pk': bucketlist.id}, format="json")
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertContains(response, bucketlist)
-
     def test_api_can_update_bucketlist(self):
         bucketlist = Bucketlist.objects.get()
         change_bucketlist = {'name' : 'Something new'}
@@ -58,6 +49,16 @@ class ViewTestCase(TestCase):
         )
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
+    def test_api_can_get_a_bucketlist(self):
+        bucketlist = Bucketlist.objects.first()
+        response = self.client.get(
+            '/bucketlists/',
+            kwargs={'pk': bucketlist.id}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+
+
+
     def test_api_can_delete_bucketlist(self):
         bucketlist = Bucketlist.objects.get()
         response = self.client.delete(
@@ -65,4 +66,3 @@ class ViewTestCase(TestCase):
             format='json',
             follow=True)
         self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
-            
--- restapi/settings.py
+++ restapi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
 
@@ -104,6 +105,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+        'DEFAULT_PERMISSIONS_CLASSES': (
+            'rest_framework.permissions.IsAuthenticated',
+            ),
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework.authentication.BasicAuthentication',
+            'rest_framework.authentication.TokenAuthentication',
+            )
+        }
 
 
 # Internationalization
